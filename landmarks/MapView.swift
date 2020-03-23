a11487421d9858650bada6a606566f74552b3575
yutz <yutz9836@gmail.com>
2020-03-23 19:08:41 +0800

    UIViewType

--- landmarks/MapView.swift
+++ landmarks/MapView.swift
@@ -15,7 +15,7 @@ struct MapView: UIViewRepresentable{
             MKMapView(frame: .zero)
     }
     
-    func updateUIView(_ uiView: MapView.UIViewType, context: UIViewRepresentableContext<MapView>) {
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
         let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
